c684f459abb5c6134bfc63d3326d70dcd207d3bf
ENG_Mostafa <mostafa.fathy22422@gmail.com>
2025-08-01 02:18:43 +0300

    startFramework3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavBar} from './components/nav-bar/nav-bar';
 import {Footer} from './components/footer/footer';
